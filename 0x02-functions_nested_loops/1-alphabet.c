a64eca565df4965bbb6b16a23c43e36e08b52ef1
rodgerswisdom <trulyhawona@gmail.com>
2023-08-16 19:00:35 -0700

    lowercase added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print alphabet in lower case using custom functions
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z': letter ++)
+		_putchar(letter)
+
+	_putchar("\n");
+}
