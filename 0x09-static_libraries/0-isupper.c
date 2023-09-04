3f0d3aaba165e7391aa932e9a89935ff2e7ab464
rodgerswisdom <trulyhawona@gmail.com>
2023-09-04 08:55:40 -0700

    ..static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * c: char to check
+ *
+ * Return: 0(if false) or 1(True)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
+}
