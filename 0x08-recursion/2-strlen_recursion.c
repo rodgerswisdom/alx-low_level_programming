929e75b1c8e03e5276be9ffdeab4321f39378129
rodgerswisdom <trulyhawona@gmail.com>
2023-10-25 20:28:46 +0300

    Do two

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *_strlen-recursion: function that returns length of string
+ *@s - the string
+ *Return: length of sting
+ * */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return 1 + int _strlen_recursion(s+1);
+}
+
