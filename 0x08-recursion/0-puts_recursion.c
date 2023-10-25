b6b674bbf71e0a51af0ef7d9bf5e684eb1a2b72d
rodgerswisdom <trulyhawona@gmail.com>
2023-10-25 19:10:04 +0300

    betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,16 @@
   *
   */
   
-  void _puts_recursion(char *s)
-  {
+void _puts_recursion(char *s)
+{
   	if(*s)
   	{
   		_putchar(*s);
   		_puts_recursion(s+1);
   	
-	  }
-	  else
-	  {
-	  	_putchar('\n');
-	  }
-  }
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
