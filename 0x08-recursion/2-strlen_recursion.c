cf27882eae265e9250760a2da532eea70d07a9d6
rodgerswisdom <trulyhawona@gmail.com>
2023-10-26 08:52:19 +0300

    4-pow_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
-/*
- *_strlen-recursion: function that returns length of string
- *@s - the string
- *Return: length of sting
- * */
+/**
+  * _strlen_recursion: function that returns length of string
+  * @s - the string
+  * Return: length of sting, or 0 when the string is empty
+  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	return (1 + int _strlen_recursion(s+1));
+	return (1 + _strlen_recursion(s + 1));
 }
-
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _pow_recursion - main function that returns power of x
+  * @x : the number whose power is returned
+  * @y : the power
+  * Return: power of x, or -1 if y is less than 0
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}	
