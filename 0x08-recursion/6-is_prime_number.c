30ec36a6d263e8172b50ff517e5d91a1349fc54e
rodgerswisdom <trulyhawona@gmail.com>
2023-10-27 12:26:00 +0300

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,7 +19,7 @@ int _prime_finder(int n, int div)
 	{
 		return (0);
 	}
-	return ( _prime_finder(n, n-1));
+	return ( _prime_finder(n, div - 1));
 }
 /**
  * is_prime_number - A function that returns 1, if prime and 0 if not prime
@@ -28,5 +28,5 @@ int _prime_finder(int n, int div)
  */
 int is_prime_number(int n)
 {
-	return (_prime_finder(n, n-1));
+	return (_prime_finder(n, n - 1));
 }
