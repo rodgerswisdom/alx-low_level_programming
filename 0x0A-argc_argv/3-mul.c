55ac2ebca6c3792e9387dda64c0a7324c03d92a7
rodgerswisdom <trulyhawona@gmail.com>
2023-10-28 04:32:42 +0300

    betty coding

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib>
 #include "main.h"
 /**
  * main - main function
@@ -11,14 +11,15 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
