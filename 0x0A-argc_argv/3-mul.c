7520a40386609d6b3031686dd1add0535e24f444
rodgerswisdom <trulyhawona@gmail.com>
2023-10-28 04:19:00 +0300

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - main function
+ * atoi - converts arguments to integer
+ * @num1: the first number
+ * @num2: the second number
+ * @result: the result of num1 * num2
+ * Return: 1 on Error, 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+}
