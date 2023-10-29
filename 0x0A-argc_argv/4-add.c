da5b681b041f8a59fa35b890af7712d5775afdcf
rodgerswisdom <trulyhawona@gmail.com>
2023-10-30 00:46:26 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,53 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-/**
- * _num_checker - functions that checks if there's a digit in a string
- * @str: array string
- * Return: always 0
- */
-int _num_checker(char *str)
-{
-	unsigned int count;
-	count = 0;
-	while (count < strlen(str))
-	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
-		count++;
-	}
-	return (1);
-}
 /**
  * main - main function
- * @argc: number of arguments
- * @argv: Array of pointers
- * Return: Always 0
+ * @sum: sum of positive numbers
+ * @argc: commands
+ * @argv: array to pointer
+ * @i: iterators
+ * @j: iterators
+ * Return 0 on success, 1 on error
  */
-int main(argc, *argv[])
+int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
-	int sum;
-	count = 1;
-	sum = 0;
-	while (count < argc)
+	int i, int j;
+	int sum = 0;
+	if (argc == 1)
 	{
-		if (_num_checker(argv[count])
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		count++;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", sum);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j])
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[i]);
+		}
+	printf("%d", sum);
 	return (0);
-	}
+}
