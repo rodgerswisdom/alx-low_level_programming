57233a3cff2527703d2568013d6af52a71201848
rodgerswisdom <trulyhawona@gmail.com>
2023-10-31 16:30:09 +0300

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,4 +35,24 @@ char *str_concat(char *s1, char *s2)
 
 	return (concat);
 }
+/**
+ * main - main function
+ * @s: string
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
 
