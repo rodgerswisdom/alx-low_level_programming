68271b4d00f2c03ca8ad13e6050edfa5a27fc947
rodgerswisdom <trulyhawona@gmail.com>
2023-11-09 02:02:22 +0300

    3-

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*func_ptr)(int, int);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+							    }
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func_ptr = get_op_func(argv[2]);
+
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+
+	result = func_ptr(num1, num2);
+	printf("%d\n", result);
+
+	return 0;
+}
