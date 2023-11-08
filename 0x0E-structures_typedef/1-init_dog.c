d958f3efe8b27af90f1812c1d142aa9a6d6caf3d
rodgerswisdom <trulyhawona@gmail.com>
2023-11-08 05:08:40 +0300

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+ * init_dog - function
+ * struct dog - struct
+ * @d: pointer
+ * @name: pointer
+ * @age: pointer
+ * @owner: pointer
+ *
+ * Description - struct and header
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner
+	}
+}
+
