83f639fd8f29a257387e7c31e0256ba86582a6ba
rodgerswisdom <trulyhawona@gmail.com>
2023-11-08 04:55:15 +0300

    betty docs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure
+ * @name: pointer to name
+ * @age: pointer to age
+ * @owner: pointer to owner
+ *
+ * Description: Structure of a dog
 struct dog
 {
 	char *name;
