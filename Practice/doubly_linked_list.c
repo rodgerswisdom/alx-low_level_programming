f2e95913c8a2fa2c05133e53282bd8617e606d4e
rodgerswisdom <trulyhawona@gmail.com>
2024-01-25 12:01:49 +0300

    Test 2

--- Practice/doubly_linked_list.c
+++ Practice/doubly_linked_list.c
@@ -2,18 +2,30 @@
 #include <stdlib.h>
 
 struct node{
+	struct node *prev;
 	int data;
+	struct node *next;
 };
 
 int main()
 {
 
+	
 	struct node mynode;
 	struct node *ptr = &mynode;
 	
+	if(mynode.prev == NULL && mynode.next == NULL)
+	{
+		struct node *prev = &mynode;
+		struct node *next = NULL;
+		
+		printf("New node with data = %d created\n", ptr->data);
+	}
 	
 	
-	ptr -> data = 12;
+	
+	
+	//ptr -> data = 12;
 	
 	printf("%d", ptr -> data);
 	return (0);
