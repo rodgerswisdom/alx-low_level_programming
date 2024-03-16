362af65f870d46a1b835a5e1bdfa293247261d3e
rodgerswisdom <trulyhawona@gmail.com>
2024-03-17 00:03:31 +0300

    Notes.c

---
+++ 0x12-singly_linked_lists/Notes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *Singly linked list
+  */
+  
+  // initialize
+  
+  typedef struct Node
+  {
+	int data;
+	struct Node *next;
+  } node_t;
+  
+  // head member that will hold a pointer to the first node
+  
+  void main(void)
+  {
+  	
+  }
+  
+  
